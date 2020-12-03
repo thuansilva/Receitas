2a6aa837f5fb5eb5c1f373dbea134a480c9cdbe5
Thuan <thuansilva14@gmail.com>
2020-12-03 18:03:28 -0300

    inserção do tema

---
+++ src/constants/theme.js
@@ -0,0 +1,36 @@
+const colors = {
+  primary: "#FFC529",
+  secondary: "#FD7A50",
+  write: "#FFFFFF",
+  back: "#201F1D",
+};
+
+const sizes = {
+  // Global
+  base: 16,
+  font: 14,
+  radius: 6,
+  padding: 25,
+  // font sizes
+  h1: 22,
+  h2: 18,
+  body: 14,
+  caption: 12,
+};
+
+const fonts = {
+  h1: {
+    fontSize: sizes.h1,
+  },
+  h2: {
+    fontSize: sizes.h2,
+  },
+  body: {
+    fontSize: sizes.body,
+  },
+  caption: {
+    fontSize: sizes.caption,
+  },
+};
+
+export { colors, sizes, font };
