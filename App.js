b9c02cc9145c4a4583d917425d6055b5b4b64ef1
Thuan <thuansilva14@gmail.com>
2020-12-07 20:09:00 -0300

    Inserção de fontes

--- App.js
+++ App.js
@@ -1,21 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { AppLoading } from "expo";
+import * as Font from "expo-font";
+
+const fetchFonts = () => {
+  return Font.loadAsync({
+    "Gilroy-Bold": require("./assets/fonts/Gilroy-Bold.ttf"),
+    "Gilroy-Heavy": require("./assets/fonts/Gilroy-Heavy.ttf"),
+    "Gilroy-Light": require("./assets/fonts/Gilroy-Light.ttf"),
+    "Gilroy-Medium": require("./assets/fonts/Gilroy-Medium.ttf"),
+    "Gilroy-Regular": require("./assets/fonts/Gilroy-Regular.ttf"),
+  });
+};
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const [isLoading, setisLoading] = useState(false);
+  if (!isLoading) {
+    return (
+      <>
+        <AppLoading
+          startAsync={fetchFonts}
+          onError={}
+          onFinish={() => setisLoading(true)}
+        />
+        <StatusBar style="auto" />
+      </>
+    );
+  }
+  return {};
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
